27fe8bfdbc60276ca0b40ac757c701d3dc664a2a
artisyn <Artioms1@gmail.com>
2023-05-09 00:18:39 +0300

    input restrictions

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ const domfiTargetTime = document.querySelector('.domfiTargetTime');
 // spread
 const domfiSpread = document.querySelector('.domfiSpread');
 // result
-const domfiTimeResult = document.querySelector('.domfiTimeResult');
+const domfiTimeResult = document.querySelector('.domfiResult');
 
 // 4eyes
 // cases
@@ -32,7 +32,7 @@ const fEyesTargetTime = document.querySelector('.fEyesTargetTime');
 // spread
 const fEyesSpread = document.querySelector('.fEyesSpread');
 // result
-const fEyesTimeResult = document.querySelector('.fEyesTimeResult');
+const fEyesTimeResult = document.querySelector('.fEyesResult');
 
 // Follow up
 // cases
@@ -44,7 +44,7 @@ const fuTargetTime = document.querySelector('.fuTargetTime');
 // spread
 const fuSpread = document.querySelector('.fuSpread');
 // result
-const fuTimeResult = document.querySelector('.fuTimeResult');
+const fuTimeResult = document.querySelector('.fuResult');
 
 // Cheques
 // cases
@@ -56,7 +56,7 @@ const scTargetTime = document.querySelector('.scTargetTime');
 // spread
 const scSpread = document.querySelector('.scSpread');
 // result
-const scTimeResult = document.querySelector('.scTimeResult');
+const scTimeResult = document.querySelector('.scResult');
 
 // Time input
 
@@ -70,6 +70,13 @@ const timeRnd = document.querySelector('.time-rnd');
 // Result
 
 const resultBtn = document.querySelector('.btn-result');
+const resultPalletCb = document.querySelector('.result__pallete-cb');
+const resultPalletDomfi = document.querySelector('.result__pallete-domfi');
+const resultPalletFeyes = document.querySelector('.result__pallete-fEyes');
+const resultPalletFu = document.querySelector('.result__pallete-fu');
+const resultPalletSc = document.querySelector('.result__pallete-sc');
+
+const timeLeft = document.querySelector('.time-left');
 
 // add or remove functions
 
@@ -138,6 +145,15 @@ const calculateTodaysTime = () => {
 const randomMinMax = (min, max) => {
 	return Math.floor(Math.random() * (max - min + 1) + min);
 };
+
+// Seconds to hours and minutes
+const secondsToHoursAndMinutes = (seconds) => {
+	let hours = Math.floor(seconds / 3600); // 1 hour = 3600 seconds
+	let minutes = Math.floor((seconds % 3600) / 60); // 1 minute = 60 seconds
+	hours = hours.toString().padStart(2, '0');
+	minutes = minutes.toString().padStart(2, '0');
+	return `${hours} : ${minutes}`;
+};
 // Adjust persentage
 
 const adjustPercent = (num, percent) => {
@@ -145,6 +161,56 @@ const adjustPercent = (num, percent) => {
 	let num2 = (100 - randomPercent) / 100;
 	return num * num2;
 };
+
+// convert time to percentage
+
+const timeToPercentage = (maxVal, actual) => {
+	return (+actual * 100) / +maxVal;
+};
+
+// allow only 0-9 to input
+const onlySingleDigit = (input, inputValue) => {
+	let last = input.value[0];
+	const regex = /^[0-9]$/;
+	if (regex.test(+inputValue)) {
+		input.value = inputValue;
+		return;
+	}
+	if (inputValue === '-') {
+		input.value = 0;
+		return;
+	}
+
+	input.value = last;
+	return;
+};
+
+// allow only 0 - 6
+const onlySingleDigitTill6 = (input, inputValue) => {
+	let last = input.value[0];
+	console.log(last);
+	const regex = /^[0-6]$/;
+	if (regex.test(+inputValue)) {
+		input.value = inputValue;
+		return;
+	}
+	if (inputValue === '-') {
+		input.value = 0;
+		return;
+	}
+	input.value = last;
+	return;
+};
+
+hours2.addEventListener('input', (e) => {
+	onlySingleDigit(e.target, e.data);
+});
+
+[minutes1, minutes2].forEach((el) =>
+	el.addEventListener('input', (e) => {
+		onlySingleDigitTill6(e.target, e.data);
+	})
+);
 
 // calculate cross border line ;
 const calculateCbTime = () => {
@@ -154,12 +220,73 @@ const calculateCbTime = () => {
 	let finalCbTime = adjustPercent(totalTime, +cbSpread.value);
 	return finalCbTime;
 };
+// calculate domfi line
+const calculateDomfiTime = () => {
+	let caseCount = +domfiCases.value;
+	let targetKpi = +domfiTargetTime.value;
+	let totalTime = caseCount * targetKpi * 60;
+	let finalDomfiTime = adjustPercent(totalTime, +domfiSpread.value);
+	return finalDomfiTime;
+};
+// calculate Feyes line
+const calculateFeyesTime = () => {
+	let caseCount = +fEyesCases.value;
+	let targetKpi = +fEyesTargetTime.value;
+	let totalTime = caseCount * targetKpi * 60;
+	let finalFeyesTime = adjustPercent(totalTime, +fEyesSpread.value);
+	return finalFeyesTime;
+};
+// calculate SC line
+const calculateSCTime = () => {
+	let caseCount = +scCases.value;
+	let targetKpi = +scTargetTime.value;
+	let totalTime = caseCount * targetKpi * 60;
+	let finalScTime = adjustPercent(totalTime, +scSpread.value);
+	return finalScTime;
+};
+// calculate FU line
+const calculateFuTime = () => {
+	let caseCount = +fuCases.value;
+	let targetKpi = +fuTargetTime.value;
+	let totalTime = caseCount * targetKpi * 60;
+	let finalFuTime = adjustPercent(totalTime, +fuSpread.value);
+	return finalFuTime;
+};
 
 const calculateFinals = () => {
 	const todaysTime = calculateTodaysTime();
+	// get total todays time
 	let finalTodaysTime = adjustPercent(todaysTime, +timeRnd.value);
-	console.log(finalTodaysTime);
-	console.log(calculateCbTime());
+	// calculate total time for each line and show it, also set the width of corresponding pallete;
+	let cbTime = calculateCbTime();
+	cbTimeResult.value = secondsToHoursAndMinutes(cbTime);
+	resultPalletCb.style.width =
+		timeToPercentage(finalTodaysTime, cbTime) + '%';
+
+	let domfiTime = calculateDomfiTime();
+	domfiTimeResult.value = secondsToHoursAndMinutes(domfiTime);
+	resultPalletDomfi.style.width =
+		timeToPercentage(finalTodaysTime, domfiTime) + '%';
+
+	let fEyesTime = calculateFeyesTime();
+	fEyesTimeResult.value = secondsToHoursAndMinutes(fEyesTime);
+	resultPalletFeyes.style.width =
+		timeToPercentage(finalTodaysTime, fEyesTime) + '%';
+
+	let fuTime = calculateFuTime();
+	fuTimeResult.value = secondsToHoursAndMinutes(fuTime);
+	resultPalletFu.style.width =
+		timeToPercentage(finalTodaysTime, fuTime) + '%';
+
+	let scTime = calculateSCTime();
+	scTimeResult.value = secondsToHoursAndMinutes(scTime);
+	resultPalletSc.style.width =
+		timeToPercentage(finalTodaysTime, scTime) + '%';
+
+	// calculate total today's time minus all the lines;
+	let monitoringTime =
+		finalTodaysTime - cbTime - domfiTime - fEyesTime - fuTime - scTime;
+	timeLeft.innerHTML = secondsToHoursAndMinutes(monitoringTime);
 };
 
 resultBtn.addEventListener('click', (e) => {
